03772e019e44d606dc11ef5465e5ded9d49b04d3
Cory Anders <coryanders@Corys-Computer.local>
2019-04-24 14:50:42 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -3,18 +3,24 @@ console.log("Up and running!");
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardTwo)
-
-if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+function checkForMatch() {
+	if (cardsInPlay.length === 2 && cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You found a match!");
 	}
 	else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.");
 	}
+}
+
+function flipCard(cardID) {
+	console.log("User flipped " + cards[cardID])
+	cardsInPlay.push(cards[cardID]);
+	checkForMatch();
+}
+
+flipCard(0);
+flipCard(2);
+
+
+
+
